91fc61ad608c7e6c59fa4b505f29fffeee027dfb
auban <auban@stag-agency.be>
2018-08-05 00:20:31 +0200

    Add mongoose Options

---
+++ mongoose/include/mongoose/mongoose.h
@@ -0,0 +1,51 @@
+/*****************************************************************//**
+* @file
+* @brief H File mongoose.h
+* @details This file is the header of the mongoose
+*
+* @author Auban le Grelle
+*
+* @date 10 mar. 2018 00:17:17
+*
+* <B>Contact:</B> auban@viamonkey.com
+*
+* @copyright (c) 2018, ViaMOnkey
+*
+* <B>Distribution:</B> This file is part of esp-idf-vmk.
+*
+*    mongoose is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Lesser General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    mongoose is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public License
+*    along with mongoose.  If not, see <http://www.gnu.org/licenses/>.
+*
+*
+*******************************************************************************/
+
+#ifndef _mongoose_mongoose_H
+#define _mongoose_mongoose_H
+
+
+#include "<mongoose.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#define MG_ENABLE_FILESYSTEM 	CONFIG_MG_ENABLE_FILESYSTEM
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _mongoose_mongoose_H */
